3f9c607b58c801e95fc25ae4f96d56358a8d69a2
srtfisher <srtfisher@users.noreply.github.com>
2020-11-11 21:23:55 +0000

    Syncing from alleyinteractive/mantle

---
+++ mu-plugins/mantle-app/routes/rest-api.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * REST API Routes
+ *
+ * @package Mantle
+ */
+
+use Mantle\Framework\Facade\Route;
+
+/*
+|--------------------------------------------------------------------------
+| REST API Routes
+|--------------------------------------------------------------------------
+|
+| Register a closure-based route:
+|
+| 	Route::get( '/closure-route', function( \WP_REST_Request $request ) {
+| 		dd( $request );
+| 	} );
+|
+*/
