8a794f0acf722fee0bc0ec73a7580df5ac3bf6a4
Sean Fisher <srtfisher@gmail.com>
2021-02-21 15:36:21 -0500

    Restore mu loader files

---
+++ mu-plugins/0-composer.php
@@ -0,0 +1,25 @@
+<?php
+/*
+	Plugin Name: Composer Loader
+	Plugin URI: https://github.com/alleyinteractive/
+	Description: A plugin to automatically load Composer dependencies.
+	Version: 0.1
+	Author: Alley Interactive
+	Author URI: http://www.alley.co/
+*/
+
+if ( ! file_exists( WP_CONTENT_DIR . '/vendor/autoload.php' ) ) {
+	add_action(
+		'admin_notices',
+		function() {
+			printf(
+				'<div class="notice notice-error"><p>%s</p></div>',
+				esc_html__( 'Composer dependencies are not installed. Run `composer install` to install them.', 'mantle' )
+			);
+		}
+	);
+
+	return;
+} else {
+	require_once WP_CONTENT_DIR . '/vendor/autoload.php';
+}
---
+++ mu-plugins/loader-mantle-app.php
@@ -0,0 +1,11 @@
+<?php
+/*
+	Plugin Name: Mantle Loader
+	Plugin URI: https://github.com/alleyinteractive/mantle
+	Description: A plugin to automatically load the Mantle framework.
+	Version: 0.1
+	Author: Alley Interactive
+	Author URI: http://www.alley.co/
+*/
+
+require_once __DIR__ . '/mantle-app/mantle.php';
